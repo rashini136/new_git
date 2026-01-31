ea8a782bfe204fbf14e9513cd0e84c2b54a575ff
Rashini Dilthara <rashinidilthara@Rashinis-MacBook-Air.local>
2026-01-31 09:05:02 +0530

    update python file with new feature, odd and even number checker

--- app.py
+++ app.py
@@ -9,4 +9,18 @@ print("I'm 20 years old")
 
 print("Enter your name")
 
+#branch -- a separate working copy of code inside the same repo
+#git branch my_secondnew_feature --- create a new branch
+#git checkout my_secondnw_feature --- switch to the new branch
+
+
+print(4+8)
+
+num1 = 4
+
+if num1%2==0:
+    print("Its an even number")
+else:
+    print("It's an odd number")
+
 
