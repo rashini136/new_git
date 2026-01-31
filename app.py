2eea89c70a12aa1c426357188087b0f2f174d421
Rashini Dilthara <rashinidilthara@Rashinis-MacBook-Air.local>
2026-01-31 08:38:45 +0530

    Update forking

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ print("Hi How are you?")
 print("Hi I'm good")
 print("Hi I'm Rashini")
 print("I'm 20 years old")
+
+
+#fork -- make your own copy of someone else's repo 
+# on your github account
+
+
